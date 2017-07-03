12f7294166c8143671149ca828aa716d78976fe9
agzuniverse <aswingauthor@gmail.com>
2017-07-03 13:17:27 +0530

    updated core files

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+		document.addEventListener("backbutton",back_handler,false);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -27,6 +28,7 @@ var app = {
 //Variable Declarations
 var rand;
 var browser;
+var div_on_focus=0;
 
 //JSON file endpoints
 var cat_end=7;
@@ -57,6 +59,56 @@ function retr(){
 	}
 }
 
+//Randomizer
 function random_gen(min_limit,max_limit){
 	return Math.floor(Math.random()*(max_limit-min_limit+1)+min_limit);
+}
+
+//Show menu
+$('#menu_button').click(function(){
+	if(!($('#menu').hasClass('visible'))){
+		$('#menu').animate({"left":"0%"},250).addClass('visible');
+		div_on_focus=1;
+	}
+});
+
+//Hide menu when tapped outside
+$('#main').click(function(){
+	div_on_focus=0;
+	$('#menu').animate({"left":"-75vw"},250).removeClass('visible');
+});
+
+//About app
+function about_app(){
+	div_on_focus=2;
+	$('#menu').animate({"left":"-75vw"},250).removeClass('visible');
+	$('#about_app').fadeIn(500);
+}
+
+//View saved content
+function saved_content(){
+}
+
+//How to contribute
+function contribute(){
+}
+
+//Handles back key
+function back_handler(){
+	if(div_on_focus==0){
+		navigator.notification.confirm('',exit_app,'Exit','Okay,Cancel');
+		function exit_app(button){
+			if(button==1){
+				navigator.app.exitApp();
+			}
+		}
+	}
+	else if(div_on_focus==1){
+		div_on_focus=0;
+		$('#menu').animate({"left":"-75vw"},250).removeClass('visible');
+	}
+	else if(div_on_focus==2){
+		div_on_focus=0;
+		$('#about_app').fadeOut(500);
+	}
 }
